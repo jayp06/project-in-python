531aa527682a0d76be08cc74c930b81e9182302d
Eduardo Malheiros <dudu_mlj@hotmail.com>
2024-11-20 18:22:51 -0300

    Criando a função visualizar treinos projeto.py
    Foi criada a função : visualizar treinos , agora os treinos adicionados podem ser visualizados

--- projeto.py
+++ projeto.py
@@ -13,3 +13,18 @@ def adicionar_treino():
         arquivo.write(f"{data},{distancia},{tempo},{localizacao},{condicoes_climaticas}\n")
 
     print("Treino adicionado com sucesso!")
+
+def visualizar_treinos():
+    try:
+        with open("treinos.txt", "r") as arquivo:
+            treinos = arquivo.readlines()
+
+            if treinos:
+                print("\n--- Treinos Registrados ---")
+                for treino in treinos:
+                    data, distancia, tempo, localizacao, condicoes_climaticas = treino.strip().split(",")
+                    print(f"Data: {data}, Distância: {distancia} km, Tempo: {tempo}, Localização: {localizacao}, Condições Climáticas: {condicoes_climaticas}")
+            else:
+                print("Nenhum treino registrado.")
+    except FileNotFoundError:
+        print("Arquivo de treinos não encontrado.")
